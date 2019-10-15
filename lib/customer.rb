cf1ebb03824ff0e7b3f344bb9358d9ffdf37913f
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-15 20:34:35 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,8 +13,12 @@ class Customer
     @@all 
   end
   
-  def meal(waiter, total, tip = 0)
+  def new_meal(waiter, total, tip = 0)
     Meal.new(waiter, self, total, tip)
+  end
+  
+  def meals
+    Meal.all.select { |meal| meal.customer == self }
   end
   
 end
